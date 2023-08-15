454c6431b07179c9197812371a02a59c3bdf3a77
Pelin Cansu Çavuş Gürcan <pelincansu.cavusgurcan@bilgeadamboost.com>
2023-08-15 10:51:42 +0300

    IBaseRepository

---
+++ InventoryManagementApp.Domain/IRepositories/IBaseRepository.cs
@@ -0,0 +1,23 @@
+﻿using InventoryManagementApp.Domain.Entities.Abstract.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagementApp.Domain.IRepositories
+{
+    public interface IBaseRepository<T> where T : IBaseEntity
+    {
+        Task Add(T item);
+        Task Update(T item);
+        Task Delete(T item);
+        Task<List<T>> GetDefaults(Expression<Func<T, bool>> expression);
+        Task<T> GetById(Expression<Func<T, bool>> expression);
+
+        Task<bool> Any(Expression<Func<T, bool>> expression);
+
+        Task<List<T>> GetAll();
+    }
+}
