9f5e0f5e79460349bd624c76323a49a6fced5ad1
Önder Şafak Şahin <ondersafak.sahin@bilgeadamboost.com>
2023-08-17 15:14:23 +0300

    DependencyInjection Class added and BOMRepository class created

---
+++ InventoryManagementApp.Infrastructure/Extensions/DependencyInjection.cs
@@ -0,0 +1,62 @@
+﻿using InventoryManagementApp.Domain.IRepositories;
+using InventoryManagementApp.Infrastructure.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagementApp.Infrastructure.Extensions
+{
+	public static class DependencyInjection
+	{
+		public static IServiceCollection AddRepositoryServices(this IServiceCollection services) 
+		{
+			//Önder
+			services.AddScoped<IBillOfMaterialRepository, BillOfMaterialRepository>();
+			//
+			//
+			//
+			//
+			//
+			//
+			//
+			//
+			//Pelin
+			//
+			//
+			//
+			//
+			//
+			//
+			//
+			//
+			//
+			//Emre
+			//
+			//
+			//
+			//
+			//
+			//
+			//
+			//Hatice
+			//
+			//
+			//
+			//
+			//
+			//
+			//
+			//Tolga
+			//
+			//
+			//
+			//
+			//
+
+			return services;
+		}
+	}
+}
