7d5b655a2e8aa1250b69a0d0a1e305b129415bb3
hzumre <hzumresonuc@gmail.com>
2023-08-17 18:05:43 +0300

    SybCategory Added.

---
+++ InventoryManagementApp.Infrastructure/Repositories/SubCategoryRepository.cs
@@ -0,0 +1,24 @@
+﻿using InventoryManagementApp.Domain.Entities.Concrete;
+using InventoryManagementApp.Domain.IRepositories;
+using InventoryManagementApp.Infrastructure.DataAccess;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagementApp.Infrastructure.Repositories
+{
+    public class SubCategoryRepository : BaseRepository<SubCategory>, ISubCategoryRepository
+    {
+        private readonly InventoryDbContext _dbContext;
+        protected DbSet<SubCategory> _subCategoryTable;
+        public SubCategoryRepository(InventoryDbContext dbContext) : base(dbContext)
+        {
+            this._dbContext = dbContext;
+            this._subCategoryTable = _dbContext.SubCategories;
+
+        }
+    }
+}
