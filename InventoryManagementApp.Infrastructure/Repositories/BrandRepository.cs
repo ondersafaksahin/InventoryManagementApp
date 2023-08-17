b2aea36737735de3ddfca65dda3534b610116f08
hzumre <hzumresonuc@gmail.com>
2023-08-17 16:21:17 +0300

    BrandRepository and DependcyInjection added

---
+++ InventoryManagementApp.Infrastructure/Repositories/BrandRepository.cs
@@ -0,0 +1,23 @@
+﻿using InventoryManagementApp.Domain.Entities.Concrete;
+using InventoryManagementApp.Domain.IRepositories;
+using InventoryManagementApp.Infrastructure.DataAccess;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagementApp.Infrastructure.Repositories
+{
+    public class BrandRepository:BaseRepository<Brand>,IBrandRepository
+    {
+        private readonly InventoryDbContext _dbContext;
+        protected DbSet<Brand> _brandTable;
+        public BrandRepository(InventoryDbContext dbContext):base(dbContext)
+        {
+            this._dbContext = dbContext;
+            this._brandTable = _dbContext.Brands;
+        }
+    }
+}
