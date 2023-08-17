3e80e9be583d9efed2222742257c38559a5ccbf2
hzumre <hzumresonuc@gmail.com>
2023-08-17 16:44:05 +0300

    ModelRepository and DependencyInjection added.

---
+++ InventoryManagementApp.Infrastructure/Repositories/ModelRepository.cs
@@ -0,0 +1,24 @@
+﻿using InventoryManagementApp.Domain.Entities.Concrete;
+using InventoryManagementApp.Domain.IRepositories;
+using InventoryManagementApp.Infrastructure.DataAccess;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagementApp.Infrastructure.Repositories
+{
+    public class ModelRepository : BaseRepository<Model>, IModelRepository
+    {
+        private readonly InventoryDbContext _dbContext;
+        protected DbSet<Model> _modelTable;
+        public ModelRepository(InventoryDbContext dbContext) : base(dbContext)
+        {
+            this._dbContext = dbContext;
+            this._modelTable = _dbContext.Models;
+          
+        }
+    }
+}
