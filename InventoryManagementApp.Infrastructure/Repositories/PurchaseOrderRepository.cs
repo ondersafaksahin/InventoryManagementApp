a695754417cb27a1c1b7b740a3b3a5c6808c5683
Pelin Cansu Çavuş Gürcan <pelincansu.cavusgurcan@bilgeadamboost.com>
2023-08-21 11:26:21 +0300

    PurchaseOrderRepository&DependencyInjection

---
+++ InventoryManagementApp.Infrastructure/Repositories/PurchaseOrderRepository.cs
@@ -0,0 +1,18 @@
+﻿using InventoryManagementApp.Domain.Entities.Concrete;
+using InventoryManagementApp.Domain.IRepositories;
+using InventoryManagementApp.Infrastructure.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagementApp.Infrastructure.Repositories
+{
+    public class PurchaseOrderRepository : BaseRepository<PurchaseOrder>, IPurchaseOrderRepository
+    {
+        public PurchaseOrderRepository(InventoryDbContext DbContext) : base(DbContext)
+        {
+        }
+    }
+}
