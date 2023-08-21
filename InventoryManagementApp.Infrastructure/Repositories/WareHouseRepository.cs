43715adcf037fffe92bf0497bf30dd158d7f9bf7
Pelin Cansu Çavuş Gürcan <pelincansu.cavusgurcan@bilgeadamboost.com>
2023-08-21 11:48:18 +0300

    Shelf, StockTransfer, WareHouse Repositories & Dependency Injection

---
+++ InventoryManagementApp.Infrastructure/Repositories/WareHouseRepository.cs
@@ -0,0 +1,18 @@
+﻿using InventoryManagementApp.Domain.Entities.Concrete;
+using InventoryManagementApp.Domain.IRepositories;
+using InventoryManagementApp.Infrastructure.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagementApp.Infrastructure.Repositories
+{
+    public class WareHouseRepository : BaseRepository<Warehouse>, IWareHouseRepository
+    {
+        public WareHouseRepository(InventoryDbContext DbContext) : base(DbContext)
+        {
+        }
+    }
+}
