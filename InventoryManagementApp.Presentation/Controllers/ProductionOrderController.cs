c2790b4cb3985ffa6dbd29fc04fc1164f72676e9
Önder Şafak Şahin <ondersafak.sahin@bilgeadamboost.com>
2023-08-31 22:54:39 +0300

    Production order basic contoller actions are completed with dummy VM classes and automapper profiles

---
+++ InventoryManagementApp.Presentation/Controllers/ProductionOrderController.cs
@@ -0,0 +1,103 @@
+﻿using AutoMapper;
+using InventoryManagementApp.Application.DTOs.ProductionOrderDTOs;
+using InventoryManagementApp.Application.Services.ProductionOrderService;
+using InventoryManagementApp.Presentation.Models.ViewModels.ProductionOrderVMs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InventoryManagementApp.Presentation.Controllers
+{
+    public class ProductionOrderController : Controller
+    {
+        IProductionOrderService _productionOrderService;
+        IMapper _mapper;
+
+        public ProductionOrderController(IProductionOrderService productionOrderService, IMapper mapper)
+        {
+            _productionOrderService = productionOrderService;
+            _mapper = mapper;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> GetAllActiveProductionOrders()
+        {
+            var pOrderList = _mapper.Map<List<ProductionOrderListVM>>(await _productionOrderService.GetDefaults(x => x.Status == Domain.Enums.Status.Active));
+            return View(pOrderList);
+        }
+
+        public async Task<IActionResult> GetAllProductionOrders()
+        {
+            var pOrderList = _mapper.Map<List<ProductionOrderListVM>>(await _productionOrderService.All());
+            return View(pOrderList);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(ProductionOrderCreateVM pOrderCreateVM)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var pOrderInfo = _mapper.Map<ProductionOrderCreateDTO>(pOrderCreateVM);
+                    await _productionOrderService.Create(pOrderInfo);
+                    return RedirectToAction("GetAllActiveProductionOrders");
+                }
+                catch (Exception ex)
+                {
+                    TempData["Error"] = ex.Message;
+                }
+            }
+            return View(pOrderCreateVM);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(int pOrderId)
+        {
+            try
+            {
+                await _productionOrderService.Delete(pOrderId);
+            }
+            catch (Exception ex)
+            {
+                TempData["Error"] = ex.Message;
+            }
+            return RedirectToAction("GetAllActiveProductionOrders");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Update(int pOrderId)
+        {
+            var pOrderVM = _mapper.Map<ProductionOrderUpdateVM>(await _productionOrderService.GetById(pOrderId));
+            return View(pOrderVM);
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Update(ProductionOrderUpdateVM pOrderUpdateVM)
+        {
+            var pOrderdto = _mapper.Map<ProductionOrderUpdateDTO>(pOrderUpdateVM);
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    await _productionOrderService.Update(pOrderdto);
+                    return RedirectToAction("GetAllActiveProductionOrders");
+                }
+                catch (Exception ex)
+                {
+                    TempData["Error"] = ex.Message;
+                }
+            }
+            return View(pOrderUpdateVM);
+        }
+    }
+}
---
+++ InventoryManagementApp.Presentation/Models/ViewModels/ProductionOrderVMs/ProductionOrderCreateVM.cs
@@ -0,0 +1,6 @@
+﻿namespace InventoryManagementApp.Presentation.Models.ViewModels.ProductionOrderVMs
+{
+    public class ProductionOrderCreateVM
+    {
+    }
+}
