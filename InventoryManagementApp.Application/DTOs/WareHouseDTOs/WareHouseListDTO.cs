6aa0aac433b028a0f98cca1a2b41319053caf46c
Pelin Cansu Çavuş Gürcan <pelincansu.cavusgurcan@bilgeadamboost.com>
2023-09-08 14:39:13 +0300

    Warehouse ListDTO-ListVM-ListDetailsView (All&Active)

--- InventoryManagementApp.Application/DTOs/WareHouseDTOs/WareHouseListDTO.cs
+++ InventoryManagementApp.Application/DTOs/WareHouseDTOs/WareHouseListDTO.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using InventoryManagementApp.Domain.Entities.Concrete;
+using InventoryManagementApp.Domain.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,31 @@ namespace InventoryManagementApp.Application.DTOs.WareHouseDTOs
 {
     public class WareHouseListDTO
     {
+
+
+        //IBaseEntity
+        public string? CreatedBy { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public string? ModifiedBy { get; set; }
+        public DateTime? ModifiedDate { get; set; }
+        public Status Status { get; set; }
+
+
+        //IEntity
+        public int ID { get; set; }
+
+
+        //Additional Properties
+
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public City? City { get; set; }
+        public string? District { get; set; }
+
+
+        //Navigation Properties
+
+        public List<Shelf>? Shelves { get; set; }
+        public List<Good>? Goods { get; set; }
     }
 }
