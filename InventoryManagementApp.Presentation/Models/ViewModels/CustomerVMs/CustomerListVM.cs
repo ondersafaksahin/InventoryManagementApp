9ae7b1b8d920bb16eccd272e2aba2c105528a35b
hzumre <hzumresonuc@gmail.com>
2023-09-08 14:13:38 +0300

    GetAllCustomers-GetAllActiveCustomers-UpdateDetails added

--- InventoryManagementApp.Presentation/Models/ViewModels/CustomerVMs/CustomerListVM.cs
+++ InventoryManagementApp.Presentation/Models/ViewModels/CustomerVMs/CustomerListVM.cs
@@ -1,6 +1,17 @@
-﻿namespace InventoryManagementApp.Presentation.Models.ViewModels.CustomerVMs
+﻿using InventoryManagementApp.Domain.Enums;
+
+namespace InventoryManagementApp.Presentation.Models.ViewModels.CustomerVMs
 {
 	public class CustomerListVM
 	{
-	}
+        public int ID { get; set; }
+        public Status Status { get; set; }
+        public string? Name { get; set; }
+        public string? Contact { get; set; }
+        public City? City { get; set; }
+        public string? Address { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Email { get; set; }
+        public string? WebPage { get; set; }
+    }
 }
