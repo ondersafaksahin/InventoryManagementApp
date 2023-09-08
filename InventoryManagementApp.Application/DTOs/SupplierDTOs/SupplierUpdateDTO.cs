5bfe2bf86020d71c0e92d11a454b4a6401c08c55
hzumre <hzumresonuc@gmail.com>
2023-09-08 15:46:40 +0300

    Supplier ListDTO-ListVM-ListDetailsView added

--- InventoryManagementApp.Application/DTOs/SupplierDTOs/SupplierUpdateDTO.cs
+++ InventoryManagementApp.Application/DTOs/SupplierDTOs/SupplierUpdateDTO.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using InventoryManagementApp.Domain.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,17 @@ namespace InventoryManagementApp.Application.DTOs.SupplierDTOs
 {
 	public class SupplierUpdateDTO
 	{
-	}
+        public int ID { get; set; }
+        public string? CreatedBy { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public string? ModifiedBy { get; set; }
+        public DateTime? ModifiedDate { get; set; }
+        public string Name { get; set; }
+        public string? Contact { get; set; }
+        public City? City { get; set; }
+        public string? Address { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string? WebPage { get; set; }
+    }
 }
