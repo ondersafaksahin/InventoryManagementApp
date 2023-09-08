b30a100c25ea60c11711761d5d024431d7cc3d3d
hzumre <hzumresonuc@gmail.com>
2023-09-08 16:36:11 +0300

    SubCategory CreateDTO-CreateVM-CreateView added

--- InventoryManagementApp.Application/DTOs/SubCategoryDTOs/SubCategoryCreateDTO.cs
+++ InventoryManagementApp.Application/DTOs/SubCategoryDTOs/SubCategoryCreateDTO.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using InventoryManagementApp.Application.DTOs.CategoryDTOs;
+using InventoryManagementApp.Domain.Entities.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,10 @@ namespace InventoryManagementApp.Application.DTOs.SubCategoryDTOs
 {
 	public class SubCategoryCreateDTO
 	{
-	}
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public int CategoryID { get; set; }
+        public Category Category { get; set; }
+        
+    }
 }
