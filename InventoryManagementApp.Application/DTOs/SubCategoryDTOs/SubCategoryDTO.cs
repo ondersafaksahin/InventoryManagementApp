434f9d7093ef11637e497dc059437990ee6a8c38
hzumre <hzumresonuc@gmail.com>
2023-09-09 09:28:26 +0300

    SubCategory ListDTO-ListVM-ListDetailsView

--- InventoryManagementApp.Application/DTOs/SubCategoryDTOs/SubCategoryDTO.cs
+++ InventoryManagementApp.Application/DTOs/SubCategoryDTOs/SubCategoryDTO.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using InventoryManagementApp.Domain.Entities.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,9 @@ namespace InventoryManagementApp.Application.DTOs.SubCategoryDTOs
 {
 	public class SubCategoryDTO
 	{
-	}
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public int CategoryID { get; set; }
+        public Category Category { get; set; }
+    }
 }
--- InventoryManagementApp.Application/DTOs/SubCategoryDTOs/SubCategoryListDTO.cs
+++ InventoryManagementApp.Application/DTOs/SubCategoryDTOs/SubCategoryListDTO.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using InventoryManagementApp.Domain.Entities.Concrete;
+using InventoryManagementApp.Domain.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,15 @@ namespace InventoryManagementApp.Application.DTOs.SubCategoryDTOs
 {
 	public class SubCategoryListDTO
 	{
-	}
+        public int ID { get; set; }
+        public string? CreatedBy { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public string? ModifiedBy { get; set; }
+        public DateTime? ModifiedDate { get; set; }
+        public Status Status { get; set; }
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public int CategoryID { get; set; }
+        public Category Category { get; set; }
+    }
 }
