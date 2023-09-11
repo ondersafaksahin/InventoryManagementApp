0861acdd079bac4b3369c49491d18ae9907a4e2f
Önder Şafak Şahin <ondersafak.sahin@bilgeadamboost.com>
2023-09-11 22:50:41 +0300

    table template design completed

--- InventoryManagementApp.Presentation/Models/ViewModels/ConversionVMs/ConversionCreateVM.cs
+++ InventoryManagementApp.Presentation/Models/ViewModels/ConversionVMs/ConversionCreateVM.cs
@@ -1,6 +1,16 @@
-﻿namespace InventoryManagementApp.Presentation.Models.ViewModels.ConversionVMs
+﻿using InventoryManagementApp.Domain.Entities.Concrete;
+using InventoryManagementApp.Domain.Enums;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace InventoryManagementApp.Presentation.Models.ViewModels.ConversionVMs
 {
     public class ConversionCreateVM
     {
+        public SelectList GoodsList { get; set; }
+        public virtual Good Good { get; set; }
+        public float BaseGoodAmount { get; set; }
+        public UnitType BaseUnit { get; set; }
+        public float FinalGoodAmount { get; set; }
+        public UnitType FinalUnit { get; set; }
     }
 }
