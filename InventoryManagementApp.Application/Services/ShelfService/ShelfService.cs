dca76532c3afd118f7041d289901169333752121
Emre Gurcan <emregurcan26@hotmail.com>
2023-10-18 22:05:08 +0300

    220 shelf entity list task and views

--- InventoryManagementApp.Application/Services/ShelfService/ShelfService.cs
+++ InventoryManagementApp.Application/Services/ShelfService/ShelfService.cs
@@ -3,6 +3,7 @@ using InventoryManagementApp.Application.DTOs.BillOfMaterialsDTOs;
 using InventoryManagementApp.Application.DTOs.ShelfDTOs;
 using InventoryManagementApp.Domain.Entities.Concrete;
 using InventoryManagementApp.Domain.IRepositories;
+using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,11 +16,13 @@ namespace InventoryManagementApp.Application.Services.ShelfService
     public class ShelfService : IShelfService
     {
         IShelfRepository _shelfRepository;
+        IWareHouseRepository _wareHouseRepository;  
         IMapper _mapper;
 
-        public ShelfService(IShelfRepository shelfRepository, IMapper mapper)
+        public ShelfService(IShelfRepository shelfRepository, IMapper mapper, IWareHouseRepository wareHouseRepository)
         {
             _shelfRepository = shelfRepository;
+            _wareHouseRepository = wareHouseRepository;
             _mapper = mapper;
         }
         public async Task<List<ShelfListDTO>> All()
@@ -38,20 +41,42 @@ namespace InventoryManagementApp.Application.Services.ShelfService
             await _shelfRepository.Delete(await _shelfRepository.GetById(x => x.ID == id));
         }
 
+
+
+
         public async Task<ShelfDTO> GetById(int id)
         {
             var shelf = await _shelfRepository.GetById(x => x.ID == id);
+
             return _mapper.Map<ShelfDTO>(shelf);
         }
 
+
+
         public async Task<List<ShelfListDTO>> GetDefaults(Expression<Func<Shelf, bool>> expression)
-        {
-            return _mapper.Map<List<ShelfListDTO>>(await _shelfRepository.GetDefaults(expression));
+        { 
+            var list = _mapper.Map<List<ShelfListDTO>>(await _shelfRepository.GetDefaults(expression));
+            var warehouse =await _wareHouseRepository.GetAll();
+
+            foreach (var item in list)
+            {
+                foreach (var wh in warehouse)
+                {
+                    if(wh.ID == item.WarehouseID)
+                    {
+                        item.Warehouse = wh;
+                    }
+                }
+            }
+        
+            return list;
+
         }
 
         public async Task Update(ShelfUpdateDTO updateDTO)
         {
             await _shelfRepository.Update(_mapper.Map<Shelf>(updateDTO));
         }
+
     }
 }
