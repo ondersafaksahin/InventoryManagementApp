74921ba77c4b1b72aebc170e8f8b4580ec203d3e
Önder Şafak Şahin <ondersafak.sahin@bilgeadamboost.com>
2023-10-19 22:51:51 +0300

    fixes

--- InventoryManagementApp.Presentation/Models/ViewModels/ShelfVMs/ShelfCreateVM.cs
+++ InventoryManagementApp.Presentation/Models/ViewModels/ShelfVMs/ShelfCreateVM.cs
@@ -1,17 +1,15 @@
 ﻿using InventoryManagementApp.Application.DTOs.WareHouseDTOs;
 using InventoryManagementApp.Domain.Entities.Concrete;
 using InventoryManagementApp.Domain.Enums;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace InventoryManagementApp.Presentation.Models.ViewModels.ShelfVMs
 {
     public class ShelfCreateVM
     {
-        public Status Status => Status.Active;
         public string Name { get; set; }
         public string? Description { get; set; }
-
         public int WarehouseID { get; set; }
-
         public List<WareHouseListDTO>? WareHouseList { get; set; }
 
     }
