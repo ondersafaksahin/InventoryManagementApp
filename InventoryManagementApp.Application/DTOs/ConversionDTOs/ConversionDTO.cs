e98e473663ee859d558e652ce6569f756e7de4e6
hzumre <hzumresonuc@gmail.com>
2023-11-07 14:52:00 +0300

    Conversion Details created

--- InventoryManagementApp.Application/DTOs/ConversionDTOs/ConversionDTO.cs
+++ InventoryManagementApp.Application/DTOs/ConversionDTOs/ConversionDTO.cs
@@ -10,12 +10,16 @@ namespace InventoryManagementApp.Application.DTOs.ConversionDTOs
 {
 	public class ConversionDTO
 	{
-        public Status Status { get; set; }
         public int ID { get; set; }
         public int GoodID { get; set; }
         public float BaseGoodAmount { get; set; }
         public UnitType BaseUnit { get; set; }
         public float FinalGoodAmount { get; set; }
         public UnitType FinalUnit { get; set; }
+        public string? CreatedBy { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public string? ModifiedBy { get; set; }
+        public DateTime? ModifiedDate { get; set; }
+        public Status Status { get; set; }
     }
 }
--- InventoryManagementApp.Presentation/Models/ViewModels/ConversionVMs/ConversionVM.cs
+++ InventoryManagementApp.Presentation/Models/ViewModels/ConversionVMs/ConversionVM.cs
@@ -1,6 +1,19 @@
-﻿namespace InventoryManagementApp.Presentation.Models.ViewModels.ConversionVMs
+﻿using InventoryManagementApp.Domain.Enums;
+
+namespace InventoryManagementApp.Presentation.Models.ViewModels.ConversionVMs
 {
     public class ConversionVM
     {
+        public int ID { get; set; }
+        public int GoodID { get; set; }
+        public float BaseGoodAmount { get; set; }
+        public UnitType BaseUnit { get; set; }
+        public float FinalGoodAmount { get; set; }
+        public UnitType FinalUnit { get; set; }
+        public string? CreatedBy { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public string? ModifiedBy { get; set; }
+        public DateTime? ModifiedDate { get; set; }
+        public Status Status { get; set; }
     }
 }
