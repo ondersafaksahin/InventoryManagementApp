ec262cb6cfc448b8e3989a2bfae6c123857d0da4
hzumre <hzumresonuc@gmail.com>
2023-11-10 15:04:49 +0300

    Category details completed

--- InventoryManagementApp.Application/DTOs/CategoryDTOs/CategoryDTO.cs
+++ InventoryManagementApp.Application/DTOs/CategoryDTOs/CategoryDTO.cs
@@ -10,8 +10,12 @@ namespace InventoryManagementApp.Application.DTOs.CategoryDTOs
 	public class CategoryDTO
 	{
         public int ID { get; set; }
+        public string? CreatedBy { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public string? ModifiedBy { get; set; }
+        public DateTime? ModifiedDate { get; set; }
+        public Status Status { get; set; }
         public string CategoryName { get; set; }
         public string? Description { get; set; }
-        public Status Status { get; set; }
     }
 }
