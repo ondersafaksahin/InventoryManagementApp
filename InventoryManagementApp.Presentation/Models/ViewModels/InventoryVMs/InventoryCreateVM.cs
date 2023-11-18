6b2c557b75bbed81f1be06cb694c17a9158800ef
Önder Şafak Şahin <ondersafak.sahin@bilgeadamboost.com>
2023-11-18 20:21:02 +0300

    fixes

--- InventoryManagementApp.Presentation/Models/ViewModels/InventoryVMs/InventoryCreateVM.cs
+++ InventoryManagementApp.Presentation/Models/ViewModels/InventoryVMs/InventoryCreateVM.cs
@@ -6,11 +6,7 @@ namespace InventoryManagementApp.Presentation.Models.ViewModels.InventoryVMs
     public class InventoryCreateVM
     {
         public float Amount { get; set; }
-        public virtual Good Good { get; set; }
         public int GoodId { get; set; }
-        public virtual Warehouse? Warehouse { get; set; }
         public int? WarehouseId { get; set; }
-        public Batch? Batch { get; set; }
-        public int? BatchId { get; set; }
     }
 }
