e83c3a16ee0da645e9c90b4ff5d02c2157d1b899
asonuc <sonucahmet@gmail.com>
2023-12-22 12:22:39 +0300

    stocktransfer create sourcewarehouse fixed

--- InventoryManagementApp.Infrastructure/Migrations/20231222090616_h1.Designer.cs
+++ InventoryManagementApp.Infrastructure/Migrations/20231222090616_h1.Designer.cs
@@ -4,6 +4,7 @@ using InventoryManagementApp.Infrastructure.DataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace InventoryManagementApp.Infrastructure.Migrations
 {
     [DbContext(typeof(InventoryDbContext))]
-    partial class InventoryDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231222090616_h1")]
+    partial class h1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ InventoryManagementApp.Infrastructure/Migrations/20231222090616_h1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InventoryManagementApp.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class h1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedDate",
+                table: "Goods",
+                type: "datetime2",
+                nullable: true,
+                defaultValue: new DateTime(2023, 12, 22, 12, 6, 16, 311, DateTimeKind.Local).AddTicks(2588),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true,
+                oldDefaultValue: new DateTime(2023, 12, 15, 13, 28, 49, 402, DateTimeKind.Local).AddTicks(6900));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedDate",
+                table: "BillOfMaterials",
+                type: "datetime2",
+                nullable: true,
+                defaultValue: new DateTime(2023, 12, 22, 12, 6, 16, 310, DateTimeKind.Local).AddTicks(3715),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true,
+                oldDefaultValue: new DateTime(2023, 12, 15, 13, 28, 49, 400, DateTimeKind.Local).AddTicks(6775));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedDate",
+                table: "Batches",
+                type: "datetime2",
+                nullable: true,
+                defaultValue: new DateTime(2023, 12, 22, 12, 6, 16, 309, DateTimeKind.Local).AddTicks(2933),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true,
+                oldDefaultValue: new DateTime(2023, 12, 15, 13, 28, 49, 398, DateTimeKind.Local).AddTicks(9822));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedDate",
+                table: "Goods",
+                type: "datetime2",
+                nullable: true,
+                defaultValue: new DateTime(2023, 12, 15, 13, 28, 49, 402, DateTimeKind.Local).AddTicks(6900),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true,
+                oldDefaultValue: new DateTime(2023, 12, 22, 12, 6, 16, 311, DateTimeKind.Local).AddTicks(2588));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedDate",
+                table: "BillOfMaterials",
+                type: "datetime2",
+                nullable: true,
+                defaultValue: new DateTime(2023, 12, 15, 13, 28, 49, 400, DateTimeKind.Local).AddTicks(6775),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true,
+                oldDefaultValue: new DateTime(2023, 12, 22, 12, 6, 16, 310, DateTimeKind.Local).AddTicks(3715));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedDate",
+                table: "Batches",
+                type: "datetime2",
+                nullable: true,
+                defaultValue: new DateTime(2023, 12, 15, 13, 28, 49, 398, DateTimeKind.Local).AddTicks(9822),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true,
+                oldDefaultValue: new DateTime(2023, 12, 22, 12, 6, 16, 309, DateTimeKind.Local).AddTicks(2933));
+        }
+    }
+}
